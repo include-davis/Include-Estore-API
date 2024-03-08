20d75f806220817155e25565f9d6b4aa1abaebd9
RayBipse <raybipse@gmail.com>
2024-03-08 08:23:55 -0800

    Fixed lint issues

--- src/services/Product.ts
+++ src/services/Product.ts
@@ -1,58 +1,58 @@
 import prisma from "../prisma/client";
 
 export default class Product {
-    // CREATE
-    static async create({ input }) {
-        const product = await prisma.product.create({
-            data: input,
-        });
-        return product;
-    }
+  // CREATE
+  static async create({ input }) {
+    const product = await prisma.product.create({
+      data: input,
+    });
+    return product;
+  }
 
-    // READ
-    static async find({ id }) {
-        return prisma.product.findUnique({ where: { id } });
-    }
+  // READ
+  static async find({ id }) {
+    return prisma.product.findUnique({ where: { id } });
+  }
 
-    static async findAll({ ids }) {
-        if (!ids) {
-            return prisma.product.findMany();
-        }
-        return prisma.product.findMany({
-            where: {
-                id: {
-                    in: ids,
-                },
-            },
-        });
+  static async findAll({ ids }) {
+    if (!ids) {
+      return prisma.product.findMany();
     }
+    return prisma.product.findMany({
+      where: {
+        id: {
+          in: ids,
+        },
+      },
+    });
+  }
 
-    // UPDATE
-    static async update({ id, input }) {
-        try {
-            const user = await prisma.user.update({
-                where: {
-                    id,
-                },
-                data: input,
-            });
-            return user;
-        } catch (e) {
-            return null;
-        }
+  // UPDATE
+  static async update({ id, input }) {
+    try {
+      const user = await prisma.user.update({
+        where: {
+          id,
+        },
+        data: input,
+      });
+      return user;
+    } catch (e) {
+      return null;
     }
+  }
 
-    // DELETE
-    static async delete({ id }) {
-        try {
-            await prisma.product.delete({
-                where: {
-                    id,
-                },
-            });
-            return true;
-        } catch (e) {
-            return false;
-        }
+  // DELETE
+  static async delete({ id }) {
+    try {
+      await prisma.product.delete({
+        where: {
+          id,
+        },
+      });
+      return true;
+    } catch (e) {
+      return false;
     }
+  }
 }
