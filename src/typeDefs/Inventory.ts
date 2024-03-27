ed069c599e12080c131b0b88bb5bb786defcd787
Kiran J <56204382+KiranJoji@users.noreply.github.com>
2024-03-27 15:32:56 -0700

    Update Inventory.ts
    Adding a way to update inventory better.

--- src/typeDefs/Inventory.ts
+++ src/typeDefs/Inventory.ts
@@ -24,13 +24,24 @@ const typeDefs = gql`
     stock_on_order: Int
   }
 
+  input UpdateInventoryInput {
+    id: ID
+    available_quantity: Int
+    cost_of_production: Int
+    lead_time: Int
+    reorder_point: Int
+    reorder_quantity: Int
+    safety_stock: Int
+    stock_on_order: Int
+  }
+
   type Query {
     inventory(id: ID!): Inventory
   }
 
   type Mutation {
     createInventory(input: InventoryInput!): Inventory
-    updateInventory(id:ID!, input: InventoryInput!): Inventory
+    updateInventory(id:ID!, input: UpdateInventoryInput!): Inventory
     deleteInventory(id: ID!): Boolean
   }
 `;
